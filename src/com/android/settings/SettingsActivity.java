Wisllley/platform_packages_apps_settings-4

Integrate Tentacles into Settings
Thanks to @camcory for the manifest part, big help there

Team-OctOS modified Dirty Tweaks for Tentacles

Change-Id: Id236e12d3f8848e926b4469b59494249284c2ded

src/com/android/settings/SettingsActivity.java
@@ -128,6 +128,8 @@ import com.android.settingslib.drawer.DashboardCategory;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 
+import com.teamoctos.tentacles.Tentacles;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -345,6 +347,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             TestingSettings.class.getName(),
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
+            Tentacles.class.getName(),
     };
 
 

Team-OctOS/platform_packages_apps_settings

Change-Id: I16a9a2b7648717b81f20667f3256413bf2ca4621

src/com/android/settings/SettingsActivity.java
@@ -137,6 +137,8 @@ import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 
 import java.net.URISyntaxException;
+import com.teamoctos.tentacles.Tentacles;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -369,7 +371,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             MasterClear.class.getName(),
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
-            AutomaticStorageManagerSettings.class.getName()
+            AutomaticStorageManagerSettings.class.getName(),
+            Tentacles.class.getName()
     };
 
 